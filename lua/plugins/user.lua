de1b4e5d86fdf3b72f1d92a9e846dfbe57a9c123
Livisu Pajares <livisupajares@gmail.com>
2025-02-17 19:42:06 -0500

    add tiny-devicons-auto-color.nvim plugin
    this pluggin makes the folder icons match with the catppuccin aesthetic inside the file tree

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -78,4 +78,12 @@ return {
       )
     end,
   },
+  {
+    "rachartier/tiny-devicons-auto-colors.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    event = "VeryLazy",
+    config = function() require("tiny-devicons-auto-colors").setup() end,
+  },
 }
